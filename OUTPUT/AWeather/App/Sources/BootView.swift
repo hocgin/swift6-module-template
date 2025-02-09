f333a64e4069b9c006e115db5288907c35e3859e
hocgin <examle@mail.com>
2025-02-09 20:51:35 +0800

    dddvvv

--- OUTPUT/AWeather/App/Sources/BootView.swift
+++ OUTPUT/AWeather/App/Sources/BootView.swift
@@ -13,45 +13,54 @@ import Voyager
 enum AppRoute: Route {
     /// 主页
     case main
-    /// 天气主页
-    case weather
     /// 添加位置
     case addLocation
     /// 设置页面
     case setting
     /// 控制台页面
-    case console
+    case control
+    case beta
     /// 日期详情
     case dayDetail(date: Date = .init(), chart: DayDetailView.Chart = .rainfall)
+    /// 天气主页
+    case weather(latitude: Decimal? = nil, longitude: Decimal? = nil)
+    /// 天气预览
+    case preview(latitude: Decimal, longitude: Decimal)
     /// 空气质量详情
     case aqiDetail
     /// 日出日落详情
     case sunDetail
     /// 恶劣天气预警详情
     case alert
+    /// 会员订阅
+    case subscription
 }
 
 struct BootView: View {
-    @EnvironmentObject private var appState: GlobalState
+    @EnvironmentObject private var globalState: GlobalState
     @StateObject var router = Router<AppRoute>(root: .main)
 
     var body: some View {
         NavVoyagerView(router: router) { route in
+            let _ = Log.ui.debug("正在进入 route = \(route)")
             switch route {
-//            case .main: MainView()
-            case .main: AddLocationView(latitude: 0, longitude: 0)
-            case .weather: MainView()
-            case .setting: SettingView()
-            case .addLocation: AddLocationView(latitude: 0, longitude: 0)
-            case .dayDetail(let date, let chart): DayDetailView(date: date, chart: chart)
-            case .aqiDetail: AQIDetailView()
-            case .sunDetail: SunDetailView()
-            case .alert: AlertView()
-            default: Text("default \(route.id)")
+                case .main: MainView()
+                case .setting: SettingView()
+                case .weather(let latitude, let longitude): MainView(latitude: latitude, longitude: longitude)
+                case .preview(let latitude, let longitude): LocationPreviewView(latitude: latitude, longitude: longitude)
+                case .addLocation: AddLocationView()
+                case .dayDetail(let date, let chart): DayDetailView(date: date, chart: chart)
+                case .aqiDetail: AQIDetailView()
+                case .sunDetail: SunDetailView()
+                case .beta: BetaView()
+                case .alert: AlertView()
+                case .subscription: AlertView()
+                default: Text("default \(route.id)")
             }
-        }.onTapGesture {
-            let _ = appState.locationKit.requestAuthorization()
-            let _ = appState.locationKit.getCurrentLocation()
+        }
+        .onAppear {
+            let _ = globalState.locationKit.requestAuthorization()
+            let _ = globalState.locationKit.getCurrentLocation()
         }
     }
 }
