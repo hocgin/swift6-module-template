a436ed31af8f535839c636d370d3586451310bd4
hocgin <hocgin@gmail.com>
2025-06-06 16:08:47 +0800

    dev

--- configure.swift
+++ configure.swift
@@ -3,99 +3,103 @@
 import Foundation
 
 func promptForVariable(variable: String, defaultValue: String) -> String {
-  if variable == "__PROJECT_NAME__", CommandLine.arguments.count == 2 {
-    return CommandLine.arguments[1]
-  }
-  print("Please enter a value for \(variable) (default: \"\(defaultValue)\")")
-  if let answer = readLine(), !answer.isEmpty {
-    return answer
-  }
-  return defaultValue
+    if variable == "__PROJECT_NAME__", CommandLine.arguments.count == 2 {
+        return CommandLine.arguments[1]
+    }
+    print("Please enter a value for \(variable) (default: \"\(defaultValue)\")")
+    if let answer = readLine(), !answer.isEmpty {
+        return answer
+    }
+    return defaultValue
 }
 
 func replaceVariablesInFiles(substitutions: [(from: String, to: String)]) {
-  let fileManager = FileManager.default
-  let enumerator = fileManager.enumerator(atPath: ".")
-  
-  while let fileName = enumerator?.nextObject() as? String {
-    var isDirectory: ObjCBool = false
-    if fileManager.fileExists(atPath: fileName, isDirectory: &isDirectory), !isDirectory.boolValue {
-      do {
-        var text = try String(contentsOfFile: fileName, encoding: .utf8)
-        for substitution in substitutions {
-          text = text.replacingOccurrences(of: substitution.from, with: substitution.to)
+    let fileManager = FileManager.default
+    let enumerator = fileManager.enumerator(atPath: ".")
+
+    while let fileName = enumerator?.nextObject() as? String {
+        var isDirectory: ObjCBool = false
+        if fileManager.fileExists(atPath: fileName, isDirectory: &isDirectory), !isDirectory.boolValue {
+            do {
+                var text = try String(contentsOfFile: fileName, encoding: .utf8)
+                for substitution in substitutions {
+                    text = text.replacingOccurrences(of: substitution.from, with: substitution.to)
+                }
+                try text.write(toFile: fileName, atomically: true, encoding: .utf8)
+            } catch {
+                // Skip this file if it isn’t valid UTF-8
+            }
         }
-        try text.write(toFile: fileName, atomically: true, encoding: .utf8)
-      } catch {
-        // Skip this file if it isn’t valid UTF-8
-      }
     }
-  }
 }
 
 func replaceVariablesInFileNames(substitutions: [(from: String, to: String)]) {
-  let fileManager = FileManager.default
-  let contents = try! fileManager.contentsOfDirectory(atPath: ".")
+    let fileManager = FileManager.default
+    let contents = try! fileManager.contentsOfDirectory(atPath: ".")
 
-  for fileName in contents {
-    if fileName == "." || fileName == ".." {
-      continue
+    for fileName in contents {
+        if fileName == "." || fileName == ".." {
+            continue
+        }
+        var newFileName = fileName
+        for substitution in substitutions {
+            newFileName = newFileName.replacingOccurrences(of: substitution.from, with: substitution.to)
+        }
+        if newFileName != fileName {
+            try! fileManager.moveItem(atPath: fileName, toPath: newFileName)
+        }
+        var isDirectory: ObjCBool = false
+        if fileManager.fileExists(atPath: newFileName, isDirectory: &isDirectory), isDirectory.boolValue {
+            fileManager.changeCurrentDirectoryPath(newFileName)
+            replaceVariablesInFileNames(substitutions: substitutions)
+            fileManager.changeCurrentDirectoryPath("..")
+        }
     }
-    var newFileName = fileName
-    for substitution in substitutions {
-      newFileName = newFileName.replacingOccurrences(of: substitution.from, with: substitution.to)
-    }
-    if newFileName != fileName {
-      try! fileManager.moveItem(atPath: fileName, toPath: newFileName)
-    }
-    var isDirectory: ObjCBool = false
-    if fileManager.fileExists(atPath: newFileName, isDirectory: &isDirectory), isDirectory.boolValue {
-      fileManager.changeCurrentDirectoryPath(newFileName)
-      replaceVariablesInFileNames(substitutions: substitutions)
-      fileManager.changeCurrentDirectoryPath("..")
-    }
-  }
 }
 
-struct Env {
-  static let ENV_VARIABLE_PREFIX = "SMT"
-  
-  static func fetchSMT(templateVarName: String, defaultValue: String, prompt: (String, String) -> String) -> String {
-    let environmentVarName = nameFor(templateVarName: templateVarName)
-    if let value = ProcessInfo.processInfo.environment[environmentVarName] {
-      return value
+enum Env {
+    static let ENV_VARIABLE_PREFIX = "SMT"
+
+    static func fetchSMT(templateVarName: String, defaultValue: String, prompt: (String, String) -> String) -> String {
+        let environmentVarName = nameFor(templateVarName: templateVarName)
+        if let value = ProcessInfo.processInfo.environment[environmentVarName] {
+            return value
+        }
+        return prompt(templateVarName, defaultValue)
     }
-    return prompt(templateVarName, defaultValue)
-  }
-  
-  static func nameFor(templateVarName: String) -> String {
-    return "\(ENV_VARIABLE_PREFIX)_\(sanitize(templateVarName: templateVarName))"
-  }
-  
-  private static func sanitize(templateVarName: String) -> String {
-    return templateVarName
-      .uppercased()
-      .replacingOccurrences(of: "\\W", with: "_", options: .regularExpression)
-      .trimmingCharacters(in: CharacterSet(charactersIn: "_"))
-  }
+
+    static func nameFor(templateVarName: String) -> String {
+        return "\(ENV_VARIABLE_PREFIX)_\(sanitize(templateVarName: templateVarName))"
+    }
+
+    private static func sanitize(templateVarName: String) -> String {
+        return templateVarName
+            .uppercased()
+            .replacingOccurrences(of: "\\W", with: "_", options: .regularExpression)
+            .trimmingCharacters(in: CharacterSet(charactersIn: "_"))
+    }
 }
 
 let yearFormatter = DateFormatter()
 yearFormatter.dateFormat = "yyyy"
 
 var substitutionPairs: [(from: String, to: String)] = [
-  (from: "xxPROJECTxNAMExx", to: "MyProject"),
-  (from: "__ORGANIZATION NAME__", to: "Awesome Org"),
-  (from: "com.AN.ORGANIZATION.IDENTIFIER", to: "com.awesome"),
-  (from: "__AUTHOR NAME__", to: "Mr McAwesome"),
-  (from: "__TODAYS_DATE__", to: DateFormatter.localizedString(from: Date(), dateStyle: .medium, timeStyle: .none)),
-  (from: "__TODAYS_YEAR__", to: yearFormatter.string(from: Date())),
-  (from: "__GITHUB_USERNAME__", to: "awesome_octocat")
+    (from: "xxPROJECTxNAMExx", to: "MyProject"),
+    (from: "__ORGANIZATION NAME__", to: "Awesome Org"),
+    (from: "com.AN.ORGANIZATION.IDENTIFIER", to: "com.awesome"),
+    (from: "__AUTHOR NAME__", to: "Mr McAwesome"),
+    (from: "__TODAYS_DATE__", to: DateFormatter.localizedString(from: Date(), dateStyle: .medium, timeStyle: .none)),
+    (from: "__TODAYS_YEAR__", to: yearFormatter.string(from: Date())),
+    (from: "__GITHUB_USERNAME__", to: "awesome_octocat")
 ]
 
 // Update the values through environment variables or prompts
 substitutionPairs = substitutionPairs.map { pair in
-  (from: pair.from, to: Env.fetchSMT(templateVarName: pair.from, defaultValue: pair.to, prompt: promptForVariable))
+    (from: pair.from, to: Env.fetchSMT(templateVarName: pair.from, defaultValue: pair.to, prompt: promptForVariable))
+}
+
+func getPair(_ fieldName: String) -> (from: String, to: String)? {
+    return substitutionPairs.first(where: { $0.from == fieldName })
 }
 
 let fileManager = FileManager.default
@@ -103,9 +107,10 @@ fileManager.changeCurrentDirectoryPath(fileManager.currentDirectoryPath)
 
 // Create OUTPUT folder and copy your template folder
 try! fileManager.createDirectory(atPath: "OUTPUT", withIntermediateDirectories: true, attributes: nil)
-try! fileManager.copyItem(atPath: "xxPROJECTxNAMExx", toPath: "OUTPUT/xxPROJECTxNAMExx")
+try! fileManager.createDirectory(atPath: "OUTPUT/.tmp", withIntermediateDirectories: true, attributes: nil)
+try! fileManager.copyItem(atPath: "xxPROJECTxNAMExx", toPath: "OUTPUT/.tmp/xxPROJECTxNAMExx")
 
-///=========== 扩展 InjectionIII ===========
+/// =========== 扩展 InjectionIII ===========
 @discardableResult
 func runShell(_ command: String) -> Int32 {
     let task = Process()
@@ -116,7 +121,7 @@ func runShell(_ command: String) -> Int32 {
     return task.terminationStatus
 }
 
-let status = runShell("cd OUTPUT/xxPROJECTxNAMExx && xcode-build-server config -workspace *.xcworkspace -scheme App && xcodebuild -resolvePackageDependencies")
+let status = runShell("cd OUTPUT/.tmp/xxPROJECTxNAMExx && xcode-build-server config -workspace *.xcworkspace -scheme App && xcodebuild -resolvePackageDependencies")
 if status != 0 {
     /// 需要 brew install xcode-build-server
     print("Failed to configure or install xcode-build-server")
@@ -124,11 +129,16 @@ if status != 0 {
     print("打开 InjectionIII，它会在右上角菜单栏中显示一个小图标，选择项目的目录，再次点击小图标，选择 Prepare Project，为项目中所有的 SwiftUI 文件添加注入代码")
     print("Read More. https://blog.imjp.uk/fxxk-xcode")
 }
-///=========== 扩展 InjectionIII ===========
+
+/// =========== 扩展 InjectionIII ===========
 
 // Move into OUTPUT and do variable replacement
-fileManager.changeCurrentDirectoryPath("OUTPUT")
+fileManager.changeCurrentDirectoryPath("OUTPUT/.tmp")
 replaceVariablesInFiles(substitutions: substitutionPairs)
 replaceVariablesInFileNames(substitutions: substitutionPairs)
+
+let pair = getPair("xxPROJECTxNAMExx")
+
+//try! fileManager.copyItem(atPath: "OUTPUT/.tmp/xxPROJECTxNAMExx", toPath: "OUTPUT/\(pair?.to)")
 
 print("Done, your project is now ready to use in the OUTPUT/ folder")
--- xxPROJECTxNAMExx/Package.swift
+++ xxPROJECTxNAMExx/Package.swift
@@ -5,34 +5,55 @@ import PackageDescription
 
 let package = Package(
     name: "xxPROJECTxNAMExx",
+    defaultLocalization: "zh",
     platforms: [
-        .iOS(.v16),
+        .iOS(.v17),
     ],
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
-            name: "xxPROJECTxNAMExx",
-            targets: ["xxPROJECTxNAMExx"]
+            name: "AppAit",
+            targets: ["AppAit"]
+        ),
+        .library(
+            name: "WidgetAit",
+            targets: ["WidgetAit"]
         ),
     ],
     dependencies: [
-        .package(url: "https://github.com/hocgin/SwiftHit", from: "0.0.49"),
-        .package(url: "https://github.com/bryan-vh/Voyager", from: "1.3.0"),
-        .package(url: "https://github.com/krzysztofzablocki/Inject", from: "1.5.2"),
+        .package(url: "git@github.com:hocgin/SwiftHit.git", revision: "1ca5df38ad1425a065961352c2e83cee069356b4"),
+        .package(url: "git@github.com:hocgin/StoreKitHelper.git", revision: "6db54ccae7a25538452e58e80c6c854a5f9c5935"),
+        .package(url: "git@github.com:hocgin/Voyager.git", revision: "aa1d0abfc6dd769f0dd6a716f355cf09cd30b437"),
+        .package(url: "https://github.com/marcprux/MemoZ.git", .upToNextMajor(from: "1.5.2")),
     ],
     targets: [
-        // Targets are the basic building blocks of a package, defining a module or a test suite.
-        // Targets can depend on other targets in this package and products from dependencies.
         .target(
-            name: "xxPROJECTxNAMExx",
+            name: "SharedAit",
             dependencies: [
-                .product(name: "SwiftHit", package: "SwiftHit"),
-                .product(name: "Voyager", package: "Voyager"),
-            ]
+                /// SwiftHit
+                .product(name: "LogHit", package: "SwiftHit"),
+                .product(name: "ExtensionHit", package: "SwiftHit"),
+                .product(name: "RequestHit", package: "SwiftHit"),
+                /// MemoZ
+                .product(name: "MemoZ", package: "MemoZ"),
+            ],
+            swiftSettings: [.swiftLanguageMode(.v5)]
         ),
-        .testTarget(
-            name: "xxPROJECTxNAMExxTests",
-            dependencies: ["xxPROJECTxNAMExx"]
+        .target(
+            name: "WidgetAit",
+            dependencies: [
+                .target(name: "SharedAit"),
+            ],
+            swiftSettings: [.swiftLanguageMode(.v5)]
+        ),
+        .target(
+            name: "AppAit",
+            dependencies: [
+                .target(name: "SharedAit"),
+                .product(name: "StoreKitHelper", package: "StoreKitHelper"),
+                .product(name: "Voyager", package: "Voyager"),
+            ],
+            swiftSettings: [.swiftLanguageMode(.v5)]
         ),
     ]
 )
