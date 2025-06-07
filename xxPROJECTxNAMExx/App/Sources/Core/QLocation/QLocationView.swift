5a6a716ae50f8727c7944fbc503862b6fdf4cd74
hocgin <hocgin@gmail.com>
2025-06-08 01:59:53 +0800

    dev

---
+++ xxPROJECTxNAMExx/App/Sources/Core/QLocation/QLocationView.swift
@@ -0,0 +1,71 @@
+//
+//  Todo.swift
+//  App
+//
+//  Created by hocgin on 2025/6/6.
+//
+import ComposableArchitecture
+import Foundation
+import SwiftUI
+
+@Reducer
+struct Tpl {
+    @ObservableState
+    struct State: Equatable, Identifiable {
+        let id: UUID = .init()
+        var isLoading: Bool = false
+    }
+
+    enum Action: BindableAction, Sendable {
+        case binding(BindingAction<State>)
+        case onAppear
+        case loaded(String)
+    }
+
+    var body: some ReducerOf<Self> {
+        BindingReducer()
+        Reduce { state, action in
+            switch action {
+            case .onAppear:
+                debugPrint("加载项 新数据..")
+                state.isLoading = true
+                return .run { send in
+                    try? await Task.sleep(nanoseconds: 6_000_000_000)
+                    await send(.loaded(UUID().uuidString))
+                }
+            case let .loaded(result):
+                debugPrint("加载完成..\(result)")
+                state.isLoading = false
+                return .none
+            default:
+                return .none
+            }
+        }
+    }
+}
+
+struct TplView: View {
+    @Bindable var store: StoreOf<Tpl>
+
+    var body: some View {
+        VStack {
+            Text("Todo.\(store.id)")
+            Text("\(store.isLoading ? "加载中" : "加载完成")")
+        }
+        .onAppear {
+            store.send(.onAppear)
+        }
+    }
+}
+
+/// =======================================================
+
+extension Tpl.State {
+    static let mock: Self = .init()
+}
+
+#Preview {
+    TplView(
+        store: Store(initialState: .mock) { Tpl() }
+    )
+}
