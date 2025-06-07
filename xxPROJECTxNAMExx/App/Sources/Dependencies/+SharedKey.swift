3ebcefba7ae9589ea42f8bd48f9df92b17020349
hocgin <hocgin@gmail.com>
2025-06-08 04:08:55 +0800

    dev

---
+++ xxPROJECTxNAMExx/App/Sources/Dependencies/+SharedKey.swift
@@ -0,0 +1,14 @@
+//
+//  Shared.swift
+//  App
+//
+//  Created by hocgin on 2025/6/8.
+//
+
+import ComposableArchitecture
+
+// extension SharedKey where Self == InMemoryKey<StackState<Boot.Path.State>>.Default {
+//    static var router: Self {
+//        Self[.inMemory("router"), default: StackState([Boot.Path.State.main])]
+//    }
+// }
