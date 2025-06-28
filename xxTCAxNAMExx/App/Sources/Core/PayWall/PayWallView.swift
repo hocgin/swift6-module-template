a768becc454f50b5c4951565f730db35b18b5087
hocgin <hocgin@gmail.com>
2025-06-28 17:07:21 +0800

    dev

--- xxTCAxNAMExx/App/Sources/Core/PayWall/PayWallView.swift
+++ xxTCAxNAMExx/App/Sources/Core/PayWall/PayWallView.swift
@@ -1,18 +1,56 @@
 //
-//  PayWallView.swift
+//  Todo.swift
 //  App
 //
-//  Created by hocgin on 2025/6/28.
+//  Created by hocgin on 2025/6/6.
 //
-
+import ComposableArchitecture
+import Foundation
 import SwiftUI
 
-struct PayWallView: View {
-    var body: some View {
-        Text("PayWallView")
+@Reducer
+struct Tpl {
+    @ObservableState
+    struct State: Equatable {}
+
+    enum Action: BindableAction, Sendable {
+        case binding(BindingAction<State>)
+        case onAppear
+    }
+
+    var body: some ReducerOf<Self> {
+        BindingReducer()
+        Reduce { _, action in
+            switch action {
+            default:
+                return .none
+            }
+        }
     }
 }
 
+struct TplView: View {
+    @Bindable var store: StoreOf<Tpl>
+
+    var body: some View {
+        VStack {
+//            Text("Todo.\(store.id)")
+//            Text("\(store.isLoading ? "加载中" : "加载完成")")
+        }
+        .onAppear {
+            store.send(.onAppear)
+        }
+    }
+}
+
+/// =======================================================
+
+extension Tpl.State {
+    static let mock: Self = .init()
+}
+
 #Preview {
-    PayWallView()
+    TplView(
+        store: Store(initialState: .mock) { Tpl() }
+    )
 }
